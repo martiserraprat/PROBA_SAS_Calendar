87ac0dcff8060924c4d87b4c30bbac2a53558b87
Marti Serra Prat / NIU1707523 <martiserraprat5@gmail.com>
2026-03-12 16:46:11 +0100

    Actualizar sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('fetch', function(event) {
-  // Esto permite que la app funcione offline en el futuro
+const CACHE_NAME = 'apex-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/app.js',
+  '/img/icon.png'
+];
+
+// Instalación
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Activación
+self.addEventListener('activate', (event) => {
+  console.log('SW activo');
+});
+
+// Estrategia de carga (Network first)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
